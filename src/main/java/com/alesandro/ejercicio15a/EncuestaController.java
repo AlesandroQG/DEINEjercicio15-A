a9d5f348bbb82efd2555b3551268f077c381f4ec
Alesandro <116791603+AlesandroQuiros@users.noreply.github.com>
2024-10-04 10:19:27 +0200

    updates

--- src/main/java/com/alesandro/ejercicio15a/EncuestaController.java
+++ src/main/java/com/alesandro/ejercicio15a/EncuestaController.java
@@ -1,8 +1,11 @@
 package com.alesandro.ejercicio15a;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
@@ -11,14 +14,17 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Clase que controla
  */
-public class EncuestaController {
+public class EncuestaController implements Initializable {
     @FXML // fx:id="cbPractica"
     private CheckBox cbPractica; // Value injected by FXMLLoader
     @FXML // fx:id="cmbEdad"
-    private ComboBox<?> cmbEdad; // Value injected by FXMLLoader
+    private ComboBox<String> cmbEdad; // Value injected by FXMLLoader
     @FXML // fx:id="lvDeportes"
     private ListView<?> lvDeportes; // Value injected by FXMLLoader
     @FXML // fx:id="rbHombre"
@@ -39,6 +45,19 @@ public class EncuestaController {
     private TextField txrProfesion; // Value injected by FXMLLoader
     @FXML // fx:id="txtHermanos"
     private TextField txtHermanos; // Value injected by FXMLLoader
+
+    /**
+     * Función que se ejecuta cuando se inicia la ventana
+     *
+     * @param location
+     * @param resources
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //ObservableList<String> edades = FXCollections.<String>observableArrayList();
+        //edades.addAll("Menores de 18", "Entre 18 y 30", "Entre 31 y 50", "Entre 51 y 70", "Mayores de 70");
+        cmbEdad.getItems().addAll("Menores de 18", "Entre 18 y 30", "Entre 31 y 50", "Entre 51 y 70", "Mayores de 70");
+    }
 
     /**
      * Función que escucha al cambio del checkbox
